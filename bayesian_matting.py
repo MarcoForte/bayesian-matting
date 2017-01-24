cb827c32edcc0a7cfc8c57ec556f940fea257207
Marco Forte <fortemarco.irl@gmail.com>
2017-01-24 15:14:03 +0000

    Added numba support to solve. Got rid of nice pythonic code but 6x speedup.

--- bayesian_matting.py
+++ bayesian_matting.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.ndimage.filters import gaussian_filter
 import cv2
+from numba import jit 
 
 from orchard_bouman_clust import clustFunc
 
@@ -34,7 +35,7 @@ def get_window(m, x, y, N):
     r[pymin:pymax, pxmin:pxmax] = m[ymin:ymax, xmin:xmax]
     return r
 
-
+@jit(nopython=True, cache=True)
 def solve(mu_F, Sigma_F, mu_B, Sigma_B, C, sigma_C, alpha_init, maxIter, minLike):
     '''
     Solves for F,B and alpha that maximize the sum of log
@@ -54,14 +55,17 @@ def solve(mu_F, Sigma_F, mu_B, Sigma_B, C, sigma_C, alpha_init, maxIter, minLike
     channel (for RGB, each of size 3x1)
     '''
     I = np.eye(3)
-    vals = []
-    FMax = 0
-    BMax = 0
+    FMax = np.zeros(3)
+    BMax = np.zeros(3)
     alphaMax = 0
     maxlike = - np.inf
     invsgma2 = 1/sigma_C**2
-    for mu_Fi, invSigma_Fi in zip(mu_F, np.linalg.inv(Sigma_F)):
-        for mu_Bj, invSigma_Bj in zip(mu_B, np.linalg.inv(Sigma_B)):
+    for i in range(mu_F.shape[0]):
+        mu_Fi = mu_F[i]
+        invSigma_Fi = np.linalg.inv(Sigma_F[i])
+        for j in range(mu_B.shape[0]):
+            mu_Bj = mu_B[j]
+            invSigma_Bj = np.linalg.inv(Sigma_B[j])
 
             alpha = alpha_init
             myiter = 1
@@ -72,25 +76,28 @@ def solve(mu_F, Sigma_F, mu_B, Sigma_B, C, sigma_C, alpha_init, maxIter, minLike
                 A12 = I*alpha*(1-alpha) * invsgma2
                 A22 = invSigma_Bj+I*(1-alpha)**2 * invsgma2
                 A = np.vstack((np.hstack((A11, A12)), np.hstack((A12, A22))))
-                b = np.array([invSigma_Fi @ mu_Fi + C*(alpha) * invsgma2, invSigma_Bj @ mu_Bj + C*(1-alpha) * invsgma2])
-                b = b.reshape(6, 1)
+                b1 = invSigma_Fi @ mu_Fi + C*(alpha) * invsgma2
+                b2 = invSigma_Bj @ mu_Bj + C*(1-alpha) * invsgma2
+                b = np.atleast_2d(np.concatenate((b1, b2))).T
 
                 X = np.linalg.solve(A, b)
                 F = np.maximum(0, np.minimum(1, X[0:3]))
                 B = np.maximum(0, np.minimum(1, X[3:6]))
                 # solve for alpha
-                alpha = np.maximum(0, np.minimum(1, ((C[:, np.newaxis]-B).T @ (F-B))/np.sum((F-B)**2)))
-                # calculate likelihood
-                L_C = - np.sum((C[:, np.newaxis]-alpha*F-(1-alpha)*B)**2) * invsgma2
-                L_F = - ((F-mu_Fi[:, np.newaxis]).T @ invSigma_Fi @ (F-mu_Fi[:, np.newaxis]))/2
-                L_B = - ((B-mu_Bj[:, np.newaxis]).T @ invSigma_Bj @ (B-mu_Bj[:, np.newaxis]))/2
-                like = (L_C + L_F + L_B)[0, 0]
+
+                alpha = np.maximum(0, np.minimum(1, ((np.atleast_2d(C).T-B).T @ (F-B))/np.sum((F-B)**2)))[0,0]
+                # # calculate likelihood
+                L_C = - np.sum((np.atleast_2d(C).T -alpha*F-(1-alpha)*B)**2) * invsgma2
+                L_F = (- ((F- np.atleast_2d(mu_Fi).T).T @ invSigma_Fi @ (F-np.atleast_2d(mu_Fi).T))/2)[0,0]
+                L_B = (- ((B- np.atleast_2d(mu_Bj).T).T @ invSigma_Bj @ (B-np.atleast_2d(mu_Bj).T))/2)[0,0]
+                like = (L_C + L_F + L_B)
+                #like = 0
 
                 if like > maxlike:
                     alphaMax = alpha
                     maxLike = like
-                    FMax = F
-                    BMax = B
+                    FMax = F.ravel()
+                    BMax = B.ravel()
 
                 if myiter >= maxIter or abs(like-lastLike) <= minLike:
                     break
@@ -183,10 +190,10 @@ def main():
     img = scipy.misc.imread("gandalf.png")[:, :, :3]
     trimap = scipy.misc.imread("gandalfTrimap.png", flatten='True')
     alpha = bayesian_matte(img, trimap)
-    scipy.misc.imsave('gandalfAlpha.png', alpha)
-    plt.title("Alpha matte")
-    plt.imshow(alpha, cmap='gray')
-    plt.show()
+    #scipy.misc.imsave('gandalfAlpha.png', alpha)
+    #plt.title("Alpha matte")
+    #plt.imshow(alpha, cmap='gray')
+    #plt.show()
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
